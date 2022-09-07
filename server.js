5b92536b9fa24928f6e45eba5ded6ef8de00f153
Keaton Sentak <ksentak01@gmail.com>
2022-09-07 18:23:29 -0400

    update server.js

--- server.js
+++ server.js
@@ -6,54 +6,54 @@ const PORT = process.env.PORT || 8080;
 require('dotenv').config();
 
 app.listen(PORT, function () {
-	console.log('Server listening on port ' + PORT);
+  console.log('Server listening on port ' + PORT);
 });
 
-app.use('/', express.static(path.join(__dirname, '/public')));
+app.use('/', express.static('public'));
 
 // Initial api search
 app.get('/api', async (req, res) => {
-	try {
-		const apiKey = process.env.IPIFYKEY;
-		const baseQuery = 'https://geo.ipify.org/api/v1?apiKey=' + apiKey;
-		const ipRes = await fetch(baseQuery);
-		const ipData = await ipRes.json();
-		res.send(ipData);
-	} catch (err) {
-		throw err;
-	}
+  try {
+    const apiKey = process.env.IPIFYKEY;
+    const baseQuery = 'https://geo.ipify.org/api/v1?apiKey=' + apiKey;
+    const ipRes = await fetch(baseQuery);
+    const ipData = await ipRes.json();
+    res.send(ipData);
+  } catch (err) {
+    throw err;
+  }
 });
 
 // Sends mapbox token
 app.get('/grabToken', (req, res) => {
-	const mapboxToken = process.env.MAPBOXTOKEN;
-	res.send({ key: mapboxToken });
+  const mapboxToken = process.env.MAPBOXTOKEN;
+  res.send({ key: mapboxToken });
 });
 
 // Ip address search
 app.get('/api/ip/:ip', async (req, res) => {
-	try {
-		const apiKey = process.env.IPIFYKEY;
-		const baseQuery = 'https://geo.ipify.org/api/v1?apiKey=' + apiKey;
-		const searchQuery = baseQuery + '&ipAddress=' + req.params.ip;
-		const ipRes = await fetch(searchQuery);
-		const ipData = await ipRes.json();
-		res.send(ipData);
-	} catch (err) {
-		throw err;
-	}
+  try {
+    const apiKey = process.env.IPIFYKEY;
+    const baseQuery = 'https://geo.ipify.org/api/v1?apiKey=' + apiKey;
+    const searchQuery = baseQuery + '&ipAddress=' + req.params.ip;
+    const ipRes = await fetch(searchQuery);
+    const ipData = await ipRes.json();
+    res.send(ipData);
+  } catch (err) {
+    throw err;
+  }
 });
 
 // Domain search
 app.get('/api/domain/:domain', async (req, res) => {
-	try {
-		const apiKey = process.env.IPIFYKEY;
-		const baseQuery = 'https://geo.ipify.org/api/v1?apiKey=' + apiKey;
-		const searchQuery = baseQuery + '&domain=' + req.params.domain;
-		const ipRes = await fetch(searchQuery);
-		const ipData = await ipRes.json();
-		res.send(ipData);
-	} catch (err) {
-		throw err;
-	}
+  try {
+    const apiKey = process.env.IPIFYKEY;
+    const baseQuery = 'https://geo.ipify.org/api/v1?apiKey=' + apiKey;
+    const searchQuery = baseQuery + '&domain=' + req.params.domain;
+    const ipRes = await fetch(searchQuery);
+    const ipData = await ipRes.json();
+    res.send(ipData);
+  } catch (err) {
+    throw err;
+  }
 });
